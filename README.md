2796c9b5533a3efd905bc659793ef402acb1a8e2
HelloKayT <katiemlim@gmail.com>
2024-06-06 23:10:40 -0700

    Add code

---
+++ README.md
@@ -0,0 +1,3 @@
+# README
+This is a copy of [this alignment extension](https://github.com/janjoerke/vscode-align-by-regex) for VSCode, modified to be able to take an argument as its regex.
+This is so I can use it in a Vim plugin key rebind to align mainly parentheses when writing Verilog
---
+++ extension.js
@@ -0,0 +1,59 @@
+'use strict';
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const block_1 = require("./block");
+const vscode = require("vscode");
+function activate(context) {
+    let lastInput;
+    let alignByRegex = vscode.commands.registerTextEditorCommand('aligner.align_by_regex', (textEditor, edit, args) => __awaiter(this, void 0, void 0, function* () {
+        let input = "";
+		if (args) {
+			let regex = args['regex'];
+			input = regex;
+		}
+		else {
+			input = yield vscode.window.showInputBox({prompt: 'Enter regular expression', value: lastInput });
+		}
+
+        if (input !== undefined && input.length > 0) {
+            lastInput = input;
+            let selection = textEditor.selection;
+            if (!selection.isEmpty) {
+                let textDocument = textEditor.document;
+                // Don't select last line, if no character of line is selected.
+                let endLine = selection.end.line;
+                let endPosition = selection.end;
+                if (endPosition.character === 0) {
+                    endLine--;
+                }
+                let range = new vscode.Range(new vscode.Position(selection.start.line, 0), new vscode.Position(endLine, textDocument.lineAt(endLine).range.end.character));
+                let text = textDocument.getText(range);
+                let block = new block_1.Block(text, input, selection.start.line, textDocument.eol).trim().align();
+                yield textEditor.edit(e => {
+                    for (let line of block.lines) {
+                        let deleteRange = new vscode.Range(new vscode.Position(line.number, 0), new vscode.Position(line.number, textDocument.lineAt(line.number).range.end.character));
+                        let replacement = '';
+                        for (let part of line.parts) {
+                            replacement += part.value;
+                        }
+                        e.replace(deleteRange, replacement);
+                    }
+                });
+            }
+        }
+    }));
+    context.subscriptions.push(alignByRegex);
+}
+exports.activate = activate;
+// this method is called when your extension is deactivated
+function deactivate() {
+}
+exports.deactivate = deactivate;
+//# sourceMappingURL=extension.js.map
